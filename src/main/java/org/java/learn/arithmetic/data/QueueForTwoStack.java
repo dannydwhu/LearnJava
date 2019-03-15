5c700728a435b55a965a7c15412cc10888bd31df
Danny Hu <347614241@qq.com>
2019-03-15 10:13:47 +0800

    test

--- src/main/java/org/java/learn/arithmetic/data/QueueForTwoStack.java
+++ src/main/java/org/java/learn/arithmetic/data/QueueForTwoStack.java
@@ -1,8 +1,5 @@
 package org.java.learn.arithmetic.data;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @auther Danny Hu
  * @date 2019/3/14
@@ -11,25 +8,25 @@ public class QueueForTwoStack {
     Stack pushStack = new Stack(100);
     Stack pullStack = new Stack(100);
 
-    public void add(int value){
+    public void add(int value) {
         pushStack.push(value);
     }
 
-    public int take(){
-        if(pullStack.size() > 0){
+    public int take() {
+        if (pullStack.size() > 0) {
             return pullStack.pop();
         } else {
-            if(pushStack.size() ==0){
+            if (pushStack.size() == 0) {
                 throw new RuntimeException("empty");
             }
-            while(pushStack.size() > 0){
+            while (pushStack.size() > 0) {
                 pullStack.push(pushStack.pop());
             }
             return take();
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         QueueForTwoStack queue = new QueueForTwoStack();
         queue.add(1);
         queue.add(2);
@@ -44,7 +41,7 @@ public class QueueForTwoStack {
         print(queue.take());
     }
 
-    public static void print(int value){
+    public static void print(int value) {
         System.out.println(value);
     }
 }
